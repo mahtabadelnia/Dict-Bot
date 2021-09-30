87a2ab10269ab79c3c60f11c8c5a73d660a22869
mahtabadelnia <44178158+mahtabadelnia@users.noreply.github.com>
2021-09-30 17:58:28 +0330

    Environment Value for Token

--- index.php
+++ index.php
@@ -10,7 +10,7 @@ try{
 	}
 include("Telegram.php");
 // Set the TOKEN
-$bot_id = "2012348496:AAHXJnqsnyXy0zrxqqVm1CmF9C13afPxBK4";
+$bot_id = getenv('TOKEN');
 // Instances the class
 $telegram = new Telegram($bot_id);
 
